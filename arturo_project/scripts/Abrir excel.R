5067446680ad457a186324e29fdc892cb11f4568
eirala88 <iralaedgar@gmail.com>
2023-04-23 21:30:21 -0400

    Excel

--- arturo_project/scripts/Abrir excel.R
+++ arturo_project/scripts/Abrir excel.R
@@ -12,3 +12,43 @@ df_2  <- import("datos/SIPAP.xlsx")
 
 df_3 <- import("datos/SIPAP.xlsx", 
                which ="Entidades Financieras 01") # Se indica el nombre de la hoja 
+
+
+#limpiar los datos(los valores N/A poner cero)
+
+df_4 %>% 
+  pivot_wider(names_from = Indice, values_from = seen)
+
+
+df_3 %>% pivot_wider(
+  names_from = Indice, 
+  values_from = seen
+  values_fill = 1
+)
+
+df_3 %>% 
+  select(Indice) %>%
+  distinct() %>% #this line removes duplicates
+  count()
+
+
+# tail() muestra las ultimas filas 
+
+tail(df_3$How.likely.are.you.to.recommend.R.to.a.colleague..friend..or.family.member., 
+     n = 20)
+
+# Cambiar el nombre de las variables
+
+df_3 = df_3 %>%
+  rename("indice" = "Nada")
+
+
+names(raw_data{1.})
+
+names(df_3[1:8])
+
+names(renamed[1:2])
+
+
+
+
